fe77aa8b1ab391723a134fad92c08a2d9abfee1f
Vinny <paulvinny@gmail.com>
2019-05-14 22:58:45 +0200

    add list method to MusicController and music.html.twig

--- src/Controller/MusicController.php
+++ src/Controller/MusicController.php
@@ -3,17 +3,35 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MusicController extends AbstractController
 {
     /**
-     * @Route("/music", name="music")
+     * Default music path
+     */
+    private const MUSIC_FOLDER = '/home/vinny/Musique';
+
+
+    /**
+     * @Route("/music/index", name="music_index")
      */
     public function index()
     {
         return $this->render('music/index.html.twig', [
             'controller_name' => 'MusicController',
         ]);
+    }
+
+    /**
+     * @Route("/music/list", name="music_list")
+     */
+    public function list()
+    {
+        $finder = new finder();
+        $finder->files()->in($this::MUSIC_FOLDER);
+
+        return $this->render('music/list.html.twig', ['music' => $finder]);
     }
 }
